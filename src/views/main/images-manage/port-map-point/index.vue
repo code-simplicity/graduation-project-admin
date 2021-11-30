f049ae46ac068cbbea83212c2b6f9a65f88d9952
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-30 22:03:48 +0800

    港口点位图

---
+++ src/views/main/images-manage/port-map-point/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>图片管理</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "PortMapPoint",
+  setup() {},
+});
+</script>
+<style scoped lang="scss"></style>
