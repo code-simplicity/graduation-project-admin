615dc549dac72cc209a1face8ebac91fe30dc2ce
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-01 22:38:07 +0800

    港口地图上传

--- src/views/main/images-manage/port-map/upload.vue
+++ src/views/main/images-manage/port-map/upload.vue
@@ -18,11 +18,11 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import Layer from "@/components/layer/index.vue";
 import { UploadFilled } from "@element-plus/icons";
 import { ElMessage } from "element-plus";
-import { uploadPortMap } from "@/api/portmap";
+import { uploadPortMap, updatePortMap } from "@/api/portmap";
 export default defineComponent({
   name: "Upload",
   components: {
@@ -51,33 +51,56 @@ export default defineComponent({
     };
   },
   methods: {
-    //   自定义提交图片
-    httpRequestPortMap(file) {
+    //  自定义提交图片
+    httpRequestPortMap(file,id) {
       // 首先就是使用formdata，将文件转为formdata类型
       const formData = new FormData();
       // 遍历文件，
       file.forEach((item) => {
         formData.append("image", item.raw);
       });
-      // 传递参数
-      uploadPortMap(formData)
-        .then((res) => {
-          ElMessage.success({
-            message: res.msg,
+      // id存在为更新港口地图
+      if (id) {
+      // 组装id
+      formData.append("id", id);
+        updatePortMap(formData)
+          .then((res) => {
+            ElMessage.success(res.msg);
+          })
+          .catch((err) => {
+            ElMessage.error({
+              message: res.msg,
+            });
           });
-        })
-        .catch((err) => {
-          ElMessage.error({
-            message: res.msg,
+      } else {
+        uploadPortMap(formData)
+          .then((res) => {
+            ElMessage.success({
+              message: res.msg,
+            });
+          })
+          .catch((err) => {
+            ElMessage.error({
+              message: res.msg,
+            });
+            console.log(`err`, err);
           });
-          console.log(`err`, err);
-        });
+      }
+      this.$emit("getTableData", true);
+      this.layerDom && this.layerDom.close();
     },
     // 提交确认
     submit() {
       if (this.uploadRef) {
-        this.httpRequestPortMap(this.uploadRef.uploadFiles);
-        this.layerDom && this.layerDom.close();
+          if (this.layer.row) {
+            this.httpRequestPortMap(this.uploadRef.uploadFiles, this.layer.row.id);
+          } else {
+              this.httpRequestPortMap(this.uploadRef.uploadFiles);
+          }
+      } else {
+           ElMessage.error({
+              message: "请选择需要上传的港口地图.",
+        });
       }
     },
   },
