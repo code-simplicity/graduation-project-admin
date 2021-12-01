83f033c44334bde41d1915a8737290102de71d78
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-01 22:37:22 +0800

    港口地图接口

--- src/api/portmap.js
+++ src/api/portmap.js
@@ -5,10 +5,11 @@ import request from "@/utils/system/request";
  * 获取港口地图
  * @returns
  */
-export const getPortMapFind = () => {
+export const getPortMapFind = (data) => {
   return request({
     url: "/portmap/find",
-    method: "get",
+    method: "post",
+    data,
   });
 };
 
@@ -19,6 +20,32 @@ export const getPortMapFind = () => {
 export const uploadPortMap = (data) => {
   return request({
     url: "/portmap/upload",
+    method: "post",
+    data,
+  });
+};
+
+/**
+ * 删除港口地图
+ * @param {*} params
+ * @returns
+ */
+export const deletePortMap = (params) => {
+  return request({
+    url: "/portmap/delete",
+    method: "get",
+    params,
+  });
+};
+
+/**
+ *修改港口地图
+ * @param {*} data
+ * @returns
+ */
+export const updatePortMap = (data) => {
+  return request({
+    url: "/portmap/update",
     method: "post",
     data,
   });
