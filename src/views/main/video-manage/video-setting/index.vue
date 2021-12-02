e9053bb61357b4e3156e8030442de4911bd400cb
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-02 09:42:26 +0800

    视频管理设置

---
+++ src/views/main/video-manage/video-setting/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>视频设置</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "VideoSetting",
+  setup() {},
+});
+</script>
+<style scoped lang="scss"></style>
