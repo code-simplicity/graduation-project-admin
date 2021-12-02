570d5fdda00ccd131e2d7227726814ac8882a2b5
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-02 09:40:22 +0800

    add content-manage of content-setting

---
+++ src/views/main/content-manage/content-setting/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>内容设置</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ContentSetting",
+  setup() {},
+});
+</script>
+<style scoped lang="scss"></style>
