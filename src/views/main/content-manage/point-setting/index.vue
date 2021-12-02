b08480ed2c9ed88075f215fc21467ce84f6ab984
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-02 09:40:47 +0800

    add content-manage of point-setting

---
+++ src/views/main/content-manage/point-setting/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>点位设置</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "PointSetting",
+  setup() {},
+});
+</script>
+<style scoped lang="scss"></style>
