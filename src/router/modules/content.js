0caf8b9d01df3605e5461722836ca476231cf6a3
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-02 09:38:18 +0800

    add content router

---
+++ src/router/modules/content.js
@@ -0,0 +1,49 @@
+import Layout from "@/layout/index.vue";
+import { createNameComponent } from "../createNode";
+
+const route = [
+  {
+    path: "/content",
+    component: Layout,
+    redirect: "/content/content-setting",
+    meta: {
+      title: "内容管理",
+      icon: "el-icon-document-copy",
+    },
+    alwayShow: true,
+    children: [
+      {
+        path: "content-setting",
+        component: createNameComponent(() =>
+          import("@/views/main/content-manage/content-setting/index.vue")
+        ),
+        meta: {
+          title: "内容设置",
+          cache: false,
+        },
+      },
+      {
+        path: "choose-setting",
+        component: createNameComponent(() =>
+          import("@/views/main/content-manage/choose-setting/index.vue")
+        ),
+        meta: {
+          title: "选择设置",
+          cache: false,
+        },
+      },
+      {
+        path: "point-setting",
+        component: createNameComponent(() =>
+          import("@/views/main/content-manage/point-setting/index.vue")
+        ),
+        meta: {
+          title: "点位设置",
+          cache: false,
+        },
+      },
+    ],
+  },
+];
+
+export default route;
