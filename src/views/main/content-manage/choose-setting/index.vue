8fa9487bc5428a26cb72ae7b165cd6c05101e3ad
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-02 09:39:52 +0800

    add content-manage of choose-seting

---
+++ src/views/main/content-manage/choose-setting/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>选择设置</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ChooseSetting",
+  setup() {},
+});
+</script>
+<style scoped lang="scss"></style>
