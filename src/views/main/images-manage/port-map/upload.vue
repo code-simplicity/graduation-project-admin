cd0d73946d7debea000b2f4621a09ca6e24b46c3
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-12-02 09:41:56 +0800

    修复图片管理中港口图片添加和修改之间代码复用逻辑出错的原因

--- src/views/main/images-manage/port-map/upload.vue
+++ src/views/main/images-manage/port-map/upload.vue
@@ -52,7 +52,7 @@ export default defineComponent({
   },
   methods: {
     //  自定义提交图片
-    httpRequestPortMap(file,id) {
+    httpRequestPortMap(file, id) {
       // 首先就是使用formdata，将文件转为formdata类型
       const formData = new FormData();
       // 遍历文件，
@@ -61,8 +61,8 @@ export default defineComponent({
       });
       // id存在为更新港口地图
       if (id) {
-      // 组装id
-      formData.append("id", id);
+        // 组装id
+        formData.append("id", id);
         updatePortMap(formData)
           .then((res) => {
             ElMessage.success(res.msg);
@@ -92,14 +92,17 @@ export default defineComponent({
     // 提交确认
     submit() {
       if (this.uploadRef) {
-          if (this.layer.row) {
-            this.httpRequestPortMap(this.uploadRef.uploadFiles, this.layer.row.id);
-          } else {
-              this.httpRequestPortMap(this.uploadRef.uploadFiles);
-          }
+        if (this.layer.row) {
+          this.httpRequestPortMap(
+            this.uploadRef.uploadFiles,
+            this.layer.row.id
+          );
+        } else {
+          this.httpRequestPortMap(this.uploadRef.uploadFiles);
+        }
       } else {
-           ElMessage.error({
-              message: "请选择需要上传的港口地图.",
+        ElMessage.error({
+          message: "请选择需要上传的港口地图.",
         });
       }
     },
