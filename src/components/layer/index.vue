3a4ff3970767140d2172c6595356941e8a7533ba
Romantic <dupyi0912@gmail.com>
2022-03-21 21:48:01 +0800

    fix：弹窗控制改变，添加销毁属性

--- src/components/layer/index.vue
+++ src/components/layer/index.vue
@@ -1,57 +1,58 @@
 <template>
-  <div v-drag>
-    <el-dialog
-      ref="dialog"
-      v-model="layer.show"
-      :title="layer.title"
-      :width="layer.width"
-      center
-    >
-      <slot></slot>
-      <template #footer v-if="layer.showButton">
-        <div>
-          <el-button type="primary" @click="confirm">确认</el-button>
-          <el-button @click="close">取消</el-button>
-        </div>
-      </template>
-    </el-dialog>
-  </div>
+	<div v-drag>
+		<el-dialog
+			ref="dialog"
+			v-model="layer.show"
+			:title="layer.title"
+			:width="layer.width"
+			center
+			destroy-on-close
+		>
+			<slot></slot>
+			<template #footer v-if="layer.showButton">
+				<div>
+					<el-button type="primary" @click="confirm">确认</el-button>
+					<el-button @click="close">取消</el-button>
+				</div>
+			</template>
+		</el-dialog>
+	</div>
 </template>
 
 <script>
 import { defineComponent, ref, watch } from "vue";
 import drag from "@/directive/drag/index";
 export default defineComponent({
-  props: {
-    layer: {
-      type: Object,
-      default: () => {
-        return {
-          show: false,
-          title: "",
-          showButton: false,
-        };
-      },
-      required: true,
-    },
-  },
-  directives: {
-    drag,
-  },
-  setup(props, ctx) {
-    const dialog = ref(null);
-    function confirm() {
-      ctx.emit("confirm");
-    }
-    function close() {
-      dialog.value.handleClose();
-    }
-    return {
-      dialog,
-      confirm,
-      close,
-    };
-  },
+	props: {
+		layer: {
+			type: Object,
+			default: () => {
+				return {
+					show: false,
+					title: "",
+					showButton: false,
+				};
+			},
+			required: true,
+		},
+	},
+	directives: {
+		drag,
+	},
+	setup(props, ctx) {
+		const dialog = ref(false);
+		function confirm() {
+			ctx.emit("confirm");
+		}
+		function close() {
+			dialog.value.handleClose();
+		}
+		return {
+			dialog,
+			confirm,
+			close,
+		};
+	},
 });
 </script>
 
